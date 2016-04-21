57ca93f478e8672046693cf4505bddc2139688aa
Kumar Limbu <kumar.limbu@nixplay.com>
2016-04-21 16:09:28 +0800

    [#118077991] Fixed bug: Mobile app permission issue with android marshmallow
    cherry picked from c9d8572ac40b9676f32d9402a2a177e6c1cd60e4

--- src/android/com/synconset/ImagePicker/ImagePicker.java
+++ src/android/com/synconset/ImagePicker/ImagePicker.java
@@ -5,15 +5,17 @@ package com.synconset;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
-
+import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.util.Log;
 
 public class ImagePicker extends CordovaPlugin {
@@ -21,12 +23,22 @@ public class ImagePicker extends CordovaPlugin {
 	 
 	private CallbackContext callbackContext;
 	private JSONObject params;
+	public static final int PERMISSION_DENIED_ERROR = 20;
+	public static final int SAVE_TO_ALBUM_SEC = 1;
+	private Intent intent;
+
+	protected final static String[] permissions = { Manifest.permission.READ_EXTERNAL_STORAGE };
+
+	protected void getReadPermission(int requestCode)
+	{
+		cordova.requestPermission(this, requestCode, Manifest.permission.READ_EXTERNAL_STORAGE);
+	}
 	 
 	public boolean execute(String action, final JSONArray args, final CallbackContext callbackContext) throws JSONException {
 		 this.callbackContext = callbackContext;
 		 this.params = args.getJSONObject(0);
 		if (action.equals("getPictures")) {
-			Intent intent = new Intent(cordova.getActivity(), MultiImageChooserActivity.class);
+			intent = new Intent(cordova.getActivity(), MultiImageChooserActivity.class);
 			int max = 20;
 			int desiredWidth = 0;
 			int desiredHeight = 0;
@@ -47,13 +59,35 @@ public class ImagePicker extends CordovaPlugin {
 			intent.putExtra("WIDTH", desiredWidth);
 			intent.putExtra("HEIGHT", desiredHeight);
 			intent.putExtra("QUALITY", quality);
-			if (this.cordova != null) {
-				this.cordova.startActivityForResult((CordovaPlugin) this, intent, 0);
+			if(cordova.hasPermission(permissions[0])) {
+				this.cordova.startActivityForResult(this, intent, 0);
+			} else if (this.cordova != null) {
+				getReadPermission(SAVE_TO_ALBUM_SEC);
 			}
+
+
 		}
 		return true;
 	}
 	
+
+	public void onRequestPermissionResult(int requestCode, String[] permissions,
+										  int[] grantResults) throws JSONException {
+		for(int r:grantResults) {
+			if(r == PackageManager.PERMISSION_DENIED) {
+				this.callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.ERROR, PERMISSION_DENIED_ERROR));
+				return;
+			}
+		}
+		switch(requestCode) {
+			case SAVE_TO_ALBUM_SEC:
+				if(intent != null) {
+					this.cordova.startActivityForResult(this, intent, 0);
+				}
+				break;
+		}
+	}
+
 	public void onActivityResult(int requestCode, int resultCode, Intent data) {
 		if (resultCode == Activity.RESULT_OK && data != null) {
 			ArrayList<String> fileNames = data.getStringArrayListExtra("MULTIPLEFILENAMES");
